5f6574d8e1c9368fbd1739a23b0d658a9807802d
Tudor <tudor.suditu@yahoo.com>
2018-05-14 00:42:18 +0300

    added dummy google map component

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="google-map" :id="mapName"></div>
+</template>
+<style scoped>
+.google-map {
+  min-width: 400px;
+  height: 600px;
+  margin: 0 auto;
+  background: gray;
+}
+</style>
+
+<script>
+export default {
+  props: ["name"],
+  data: function() {
+    return {
+      mapName: `${this.name}-map`
+    };
+  },
+  mounted: function() {
+    const element = document.getElementById(this.mapName);
+    const options = {
+      zoom: 14,
+      center: new google.maps.LatLng(45.6428907,25.5916801)
+    };
+    const map = new google.maps.Map(element, options);
+  }
+};
+</script>
