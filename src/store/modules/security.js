4d1170212ea3b76ac253a55059817618c2a5af43
Tudor <tudor.suditu@yahoo.com>
2018-05-14 18:39:15 +0300

    define add user action / mutation

--- src/store/modules/security.js
+++ src/store/modules/security.js
@@ -3,30 +3,45 @@ import axios from 'axios'
 import store from '../../store'
 import { debug } from 'util';
 
-const state = { 
-    auth: undefined
+const state = {
+    auth: undefined,
+    userInfo: undefined
 }
 
 const getters = {
-    auth: state => state.auth 
+    auth: state => state.auth,
+    userInfo: state => state.userInfo
 }
 
 const actions = {
-    authLogin ({ commit }, keycloakAuth) {
+    authLogin({ commit }, keycloakAuth) {
         store.commit(types.SECURITY_AUTH, keycloakAuth)
-    }
+    },
+    loadUserInfo({ commit, getters, rootGetters }, payload) {
+        state.auth
+            .loadUserInfo()
+            .success(function (userInfo) {
+                commit(types.ADD_USER_INFO, userInfo);
+            })
+            .error(function (error) {
+                console.log(error);
+            });
+    },
 }
 
 const mutations = {
-    [types.SECURITY_AUTH] (state, keycloakAuth) { 
+    [types.SECURITY_AUTH](state, keycloakAuth) {
         state.auth = keycloakAuth
         // axios.defaults.headers.common = { 'Authorization': 'Bearer ' + keycloakAuth.token }
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + keycloakAuth.token
         console.log('Token', axios.defaults.headers.common['Authorization']);
+    },
+    [types.ADD_USER_INFO](state, userInfo) {
+        state.userInfo = userInfo;
     }
 }
 
-export default { 
+export default {
     state,
     actions,
     getters,
