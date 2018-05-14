35868379228c1f2ca8b66075a9d5f72710cc1e4b
Tudor <tudor.suditu@yahoo.com>
2018-05-15 00:05:29 +0300

    refined google map to take user location. emit events when current location is changed

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="google-map" :id="mapName"></div>
+  <div class="google-map" :id="mapName"></div>
 </template>
 <style scoped>
 .google-map {
@@ -15,16 +15,56 @@ export default {
   props: ["name"],
   data: function() {
     return {
-      mapName: `${this.name}-map`
+      mapName: `${this.name}-map`,
+      map: undefined,
+      currentPositionMarker: undefined,
+      currentPosition: undefined
     };
   },
   mounted: function() {
     const element = document.getElementById(this.mapName);
     const options = {
-      zoom: 14,
-      center: new google.maps.LatLng(45.6428907,25.5916801)
+      center: new google.maps.LatLng(45.6428907, 25.5916801),
+      zoom: 15
     };
-    const map = new google.maps.Map(element, options);
+
+    this.map = new google.maps.Map(element, options);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.locationSuccessHandler,
+        this.locationErrorHandler
+      );
+    }
+  },
+  methods: {
+    locationErrorHandler: function(error) {
+      console.log(error);
+    },
+    locationSuccessHandler: function(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const newCenter = new google.maps.LatLng(latitude, longitude);
+      this.currentPositionMarker = new google.maps.Marker({
+        position: newCenter,
+        map: this.map,
+        title: "Your current position",
+        draggable: true
+      });
+
+      this.map.panTo(newCenter);
+      this.map.setZoom(17);
+
+      //listen for current location marker drag
+      google.maps.event.addListener(
+        this.currentPositionMarker,
+        "dragend",
+        () => {
+          this.currentPosition = this.currentPositionMarker.getPosition();
+          this.$emit("currentLocationChanged", this.currentPosition);
+        }
+      );
+    }
   }
 };
 </script>
