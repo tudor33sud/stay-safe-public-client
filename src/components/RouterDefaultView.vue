f70928da4476be2b1f89a87357daaaeb702abd26
Tudor <tudor.suditu@yahoo.com>
2018-05-16 23:31:35 +0300

    added tracking route

--- src/components/RouterDefaultView.vue
+++ src/components/RouterDefaultView.vue
@@ -12,7 +12,7 @@ module.exports = {
     defaultView: function() {
       if (this.auth) {
         if (this.auth.hasResourceRole("ambulance", "stay-safe-api")) {
-          return this.$router.push("/activity");
+          return this.$router.push("/tracking");
         } else if (this.auth.hasResourceRole("dispatcher", "stay-safe-api")) {
           return this.$router.push("/assignments");
         } else {
