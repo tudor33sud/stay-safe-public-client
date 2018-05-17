218385bb7e5607877209a4ed339670320bc19ed8
Tudor <tudor.suditu@yahoo.com>
2018-05-18 01:37:36 +0300

    tweaked google maps to support all use cases : tracking / reporting / live mode. Fixed zoom levels and map initial state.

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -13,12 +13,23 @@
 
 <script>
 export default {
+  watch: {
+    syncedMarkers: function(newVal, oldVal) {
+      this.registerMarkers(newVal, false);
+    }
+  },
   props: {
     name: {
       type: String,
       required: true
     },
     markers: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    syncedMarkers: {
       type: Array,
       default: function() {
         return [];
@@ -31,6 +42,14 @@ export default {
     syncGeolocation: {
       type: Boolean,
       default: false
+    },
+    geolocation: {
+      type: Boolean,
+      default: true
+    },
+    geolocationIcon: {
+      type: String,
+      default: undefined
     }
   },
   data: function() {
@@ -39,42 +58,39 @@ export default {
       map: undefined,
       currentPositionMarker: undefined,
       currentPosition: undefined,
-      locationWatcher: null
+      locationWatcher: null,
+      gmapSyncedMarkers: [],
+      gmapStaticMarkers: []
     };
   },
   mounted: function() {
-    if (!navigator.geolocation) {
-      alert("Sorry you do not support geolocation");
-      return;
-    }
     const element = document.getElementById(this.mapName);
     const options = {
       center: new google.maps.LatLng(45.6428907, 25.5916801),
-      zoom: 10
+      zoom: 5
     };
 
     this.map = new google.maps.Map(element, options);
 
-    navigator.geolocation.getCurrentPosition(
-      this.locationSuccessHandler,
-      this.locationErrorHandler
-    );
-
-    this.markers.forEach(marker => {
-      const markerPosition = new google.maps.LatLng(marker.lat, marker.lng);
-      const gmapMarker = new google.maps.Marker({
-        position: markerPosition,
-        map: this.map,
-        title: marker.title ? marker.title : undefined
-      });
-    });
+    if (this.geolocation === true) {
+      //if geolocation enabled get user's current position
+      if (!navigator.geolocation) {
+        alert("Sorry you do not support geolocation");
+        return;
+      }
+      navigator.geolocation.getCurrentPosition(
+        this.locationSuccessHandler,
+        this.locationErrorHandler
+      );
+    }
+    this.registerMarkers(this.markers);
   },
   methods: {
     locationErrorHandler: function(error) {
       console.log(error);
     },
     locationSuccessHandler: function(position) {
-      this.changeCurrentLocation({
+      this.changeCurrentPosition({
         lat: position.coords.latitude,
         lng: position.coords.longitude
       });
@@ -87,25 +103,29 @@ export default {
         map: this.map,
         title: "Your current position",
         draggable: this.currentLocationDraggable,
-        animation: google.maps.Animation.DROP
+        animation: google.maps.Animation.DROP,
+        icon: this.geolocationIcon ? this.geolocationIcon : undefined
       });
 
       this.map.panTo(newCenter);
-      this.map.setZoom(17);
+      this.map.setZoom(15);
 
       //listen for current location marker drag
       google.maps.event.addListener(
         this.currentPositionMarker,
         "dragend",
         () => {
-          this.changeCurrentLocation({
+          this.changeCurrentPosition({
             lat: this.currentPositionMarker.getPosition().lat(),
             lng: this.currentPositionMarker.getPosition().lng()
           });
         }
       );
 
+      this.addPanOnClickListener(this.currentPositionMarker);
+
       if (this.syncGeolocation) {
+        //change this to reuse getPosition instead of the watcher
         const options = {
           enableHighAccuracy: false,
           timeout: 5000,
@@ -117,14 +137,14 @@ export default {
           options
         );
         //test method
-        // setInterval(() => {
-        //   this.onLocationWatchSuccess({
-        //     coords: {
-        //       latitude: this.currentPosition.lat + 0.0001,
-        //       longitude: this.currentPosition.lng + 0.0001
-        //     }
-        //   });
-        // }, 1000);
+        setInterval(() => {
+          this.onLocationWatchSuccess({
+            coords: {
+              latitude: this.currentPosition.lat + 0.0001,
+              longitude: this.currentPosition.lng + 0.0001
+            }
+          });
+        }, 3000);
       }
     },
     onLocationWatchSuccess: function(position) {
@@ -132,7 +152,7 @@ export default {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      this.changeCurrentLocation(latLngPosition);
+      this.changeCurrentPosition(latLngPosition);
       const changedMarkerPosition = new google.maps.LatLng(
         latLngPosition.lat,
         latLngPosition.lng
@@ -142,9 +162,54 @@ export default {
     onLocationWatchError: function(err) {
       console.log(err);
     },
-    changeCurrentLocation: function(position) {
+    changeCurrentPosition: function(position) {
       this.currentPosition = position;
       this.$emit("currentLocationChanged", position);
+    },
+    registerMarkers: function(markers, staticMarkers = true) {
+      if (staticMarkers !== true) {
+        this.gmapSyncedMarkers.forEach(marker => {
+          marker.setMap(null);
+          google.maps.event.clearInstanceListeners(marker);
+        });
+      }
+      const gmapsMarkers = markers.map(marker => {
+        const markerPosition = new google.maps.LatLng(marker.lat, marker.lng);
+        const gmapMarker = new google.maps.Marker({
+          position: markerPosition,
+          map: this.map,
+          title: marker.title ? marker.title : undefined,
+          icon: marker.icon ? marker.icon : undefined
+        });
+        this.addPanOnClickListener(gmapMarker);
+        return gmapMarker;
+      });
+      if (staticMarkers === true) {
+        this.gmapStaticMarkers = gmapsMarkers;
+      } else {
+        this.gmapSyncedMarkers = gmapsMarkers;
+      }
+      const bounds = new google.maps.LatLngBounds();
+      const allMarkers = [...this.gmapSyncedMarkers, ...this.gmapStaticMarkers];
+      for (var i = 0; i < allMarkers.length; i++) {
+        bounds.extend(allMarkers[i].getPosition());
+      }
+      if (allMarkers.length > 1) {
+        this.map.fitBounds(bounds);
+      } else if (allMarkers.length == 1) {
+        this.map.setCenter(bounds.getCenter());
+        this.map.setZoom(17);
+      }
+    },
+    addPanOnClickListener: function(marker) {
+      google.maps.event.addListener(marker, "click", () => {
+        const latLng = new google.maps.LatLng(
+          marker.getPosition().lat(),
+          marker.getPosition().lng()
+        );
+        this.map.panTo(latLng);
+        this.map.setZoom(17);
+      });
     }
   }
 };
