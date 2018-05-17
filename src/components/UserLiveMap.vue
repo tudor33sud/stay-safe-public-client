5d8eaa6607fd892aa9cb6055888d257b19d3871f
Tudor <tudor.suditu@yahoo.com>
2018-05-18 01:38:41 +0300

    added user live map to be used when reporting

---
+++ src/components/UserLiveMap.vue
@@ -0,0 +1,79 @@
+<template>
+    <googlemap name="livemap" :geolocation="false" :markers="targetMarkerArray" :syncedMarkers="ambulanceMarkerObject">
+    </googlemap>
+</template>
+
+<style lang="sass" scoped>
+#livemap-map{
+    height:100%;
+}
+</style>
+
+<script>
+import { mapGetters } from "vuex";
+import * as liveFeedService from "../service/live-feed";
+import * as eventService from "../service/events";
+const ambulanceIcon = "/static/assets/ambulance.png";
+function getMarkerFromParsedMessage(parsed) {
+  const { lat, lng } = parsed.value;
+  return {
+    lat,
+    lng,
+    title: "ambulance",
+    icon: ambulanceIcon
+  };
+}
+module.exports = {
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters({
+      auth: "auth"
+    })
+  },
+  mounted() {
+    this.initWebSocket(this.event.id);
+  },
+  data() {
+    return {
+      trackingWS: null,
+      ambulanceMarkerObject: [],
+      targetMarkerArray: [
+        Object.assign(eventService.getLatLon(this.event), {
+          title: "Event location"
+        })
+      ]
+    };
+  },
+  methods: {
+    initWebSocket(eventId) {
+      this.trackingWS = new WebSocket(
+        `ws://localhost:8999?auth=${this.auth.token}&eventId=${eventId}`
+      );
+      this.trackingWS.onopen = e => {
+        //console.log("opened");
+      };
+      this.trackingWS.onclose = e => {
+        alert("ws closed");
+      };
+      this.trackingWS.onmessage = message => {
+        this.onMessage(message);
+      };
+    },
+    onMessage: function(message) {
+      try {
+        const parsed = JSON.parse(message.data);
+        debugger;
+        if (parsed.type === "ambLocUpdate") {
+          this.ambulanceMarkerObject = [getMarkerFromParsedMessage(parsed)];
+        }
+      } catch (err) {}
+    }
+  }
+};
+</script>
+
