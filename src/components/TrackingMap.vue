0c00a5c1dd5a7515edb95d67605f54d96b1be6bf
Tudor <tudor.suditu@yahoo.com>
2018-05-20 18:36:31 +0300

    tracking map fully customized to show current event and end it

--- src/components/TrackingMap.vue
+++ src/components/TrackingMap.vue
@@ -1,11 +1,104 @@
 <template>
-  <googlemap name="tracking" :currentLocationDraggable="false" :geolocationIcon="getAmbulanceIcon()" :markers="[targetMarker]" :syncGeolocation="true" @currentLocationChanged="currentLocationChanged">
-  </googlemap>
+  <div style="height:100%">
+    <googlemap name="tracking" :currentLocationDraggable="false" :geolocationIcon="getAmbulanceIcon()" :markers="[targetMarker]" :syncGeolocation="true" @currentLocationChanged="currentLocationChanged">
+    </googlemap>
+    <div class="details-container" :class="{fullscreen:detailsFullScreen}">
+      <div v-if="detailsFullScreen" class="event-details">
+        <div class="event-detail-item">
+          <md-icon>add_alarm</md-icon> {{formatDate(event.createdAt)}}
+        </div>
+        <div class="event-detail-item">
+          <md-icon>account_circle</md-icon> {{event.requester.display}}
+        </div>
+        <div class="event-detail-item">
+          <md-icon>attachment</md-icon>
+          <span> Attached images</span>
+          <div class="md-layout">
+            <div v-for="attachmentData in attachmentsData" class="md-layout-item md-size-25 md-medium-size-50 image-wrapper">
+              <img style="width:100%;height:100%;" :src="attachmentData" alt="event pictue">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <md-button class="md-raised md-icon-button btn-details" @click="detailsFullScreen = !detailsFullScreen">
+        <md-icon>unfold_more</md-icon>
+      </md-button>
+      <md-button class="md-raised md-icon-button btn-success md-primary" @click="finishEvent(event)">
+        <md-icon>check</md-icon>
+      </md-button>
+    </div>
+  </div>
+
 </template>
 
 <style lang="sass" scoped>
 #tracking-map{
   height:100%;
+}
+
+$text-color-white: #fff;
+$text-color-black: #000;
+.details-container{
+  .event-details{
+    width:100%;
+    color:$text-color-white;
+    font-size:16px;
+    padding: 24px;
+    padding-right:0;
+
+    .event-detail-item {
+      margin-bottom: 8px;
+
+      .md-layout {
+        margin-top: 18px;
+        margin-right: 60px;
+      }
+
+      span {
+        margin-bottom: 18px;
+      }
+
+      .image-wrapper{
+        padding-right: 18px;
+        padding-bottom: 18px;
+        img {
+         border-radius: 36px;
+        }
+      }
+    }
+    .md-icon{
+      color:$text-color-white;
+    }
+    height: 100%; 
+    overflow:auto;
+  }
+  background-color: rgba(0,0,0, 0.85);
+
+  &.fullscreen{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    .md-icon-button{
+      right:30px;
+    }
+  }
+
+  .md-icon-button{
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    /* left: 0; */
+    right: 8px;
+    bottom: 90px;
+    &.btn-success {
+      bottom: 40px;
+    }
+
+
+  }
 }
 </style>
 
@@ -13,6 +106,8 @@
 import { mapGetters } from "vuex";
 import * as liveFeedService from "../service/live-feed";
 import * as eventService from "../service/events";
+import * as trackingService from "../service/tracking";
+import moment from "moment";
 const ambulanceIcon = "/static/assets/ambulance.png";
 module.exports = {
   props: {
@@ -28,6 +123,14 @@ module.exports = {
   },
   mounted() {
     this.initWebSocket(this.event.id);
+    this.getAttachment(this.event);
+  },
+  beforeDestroy() {
+    if (this.trackingWS != null) {
+      if (this.trackingWS.readyState == this.trackingWS.OPEN) {
+        this.trackingWS.close();
+      }
+    }
   },
   data() {
     return {
@@ -35,7 +138,9 @@ module.exports = {
       currentLocation: null,
       targetMarker: Object.assign(eventService.getLatLng(this.event), {
         title: "target location"
-      })
+      }),
+      detailsFullScreen: false,
+      attachmentsData: []
     };
   },
   methods: {
@@ -58,6 +163,9 @@ module.exports = {
       const { lat, lng } = location;
       liveFeedService.sendLocation(this.trackingWS, lat, lng);
     },
+    sendEventFinished: function(eventId) {
+      liveFeedService.sendEventFinished(this.trackingWS, eventId);
+    },
     currentLocationChanged: function(position) {
       this.currentLocation = position;
       if (this.trackingWS.readyState !== this.trackingWS.OPEN) {
@@ -65,8 +173,41 @@ module.exports = {
       }
       this.sendLocation(position);
     },
+    getAttachment: function(event) {
+      const getAttachmentsRequests = event.attachments.map(attachment => {
+        return eventService.getAttachment(event.id, attachment.id);
+      });
+      Promise.all(getAttachmentsRequests)
+        .then(responses => {
+          this.attachmentsData = responses.map(response => {
+            const imageData =
+              "data:image/png;base64, " +
+              new Buffer(response.data, "binary").toString("base64");
+            return imageData;
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    finishEvent: function(event) {
+      trackingService
+        .finishEvent(event.id)
+        .then(response => {
+          return this.sendEventFinished(event.id);
+        })
+        .then(() => {
+          this.$emit("eventFinished", event.id);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     getAmbulanceIcon: function() {
       return ambulanceIcon;
+    },
+    formatDate(dateString) {
+      return moment(dateString).fromNow();
     }
   }
 };
