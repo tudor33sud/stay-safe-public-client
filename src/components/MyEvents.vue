f7d9c7ee12d967e85dbbe31dd1607daf611e6173
Tudor <tudor.suditu@yahoo.com>
2018-05-23 00:31:44 +0300

    added empty state. fixed live button to appear on busy and requested statuses

--- src/components/MyEvents.vue
+++ src/components/MyEvents.vue
@@ -1,49 +1,52 @@
 <template>
-    <div>
-        <div class="md-layout">
-            <div v-if="!eventLive" v-for="event in myEvents" :key="event.id" class="cards-layout md-layout-item md-size-25 md-medium-size-50 md-xsmall-size-100">
-                <md-card class="md-card-example">
-                    <md-card-area md-inset>
-                        <md-card-header>
-                            <h2 class="md-title">{{event.requester.display}}</h2>
-                            <div class="md-subhead">
-                                <md-icon>location_on</md-icon>
-                                <span>{{event.location}}</span>
-                            </div>
-                        </md-card-header>
-                    </md-card-area>
-
-                    <md-card-content>
-                        <h3 class="md-subheading">Details</h3>
-                        <div class="card-details">
-                            <md-icon>access_time</md-icon>
-                            <div class="md-button-group">
-                                <span class="description-item">{{formatDate(event.createdAt)}}</span>
-                            </div>
-                        </div>
-                        <div class="card-details">
-                            <md-icon>local_offer</md-icon>
-                            <div class="md-button-group">
-                                <md-chip v-for="tag in event.tags" :key="tag.name" :class="getGravityClass(tag.gravity)">
-                                    {{tag.name}}
-                                </md-chip>
-                                <!-- <md-chip>Car crash</md-chip>
-                                <md-chip class="md-accent">SOS</md-chip> -->
-                            </div>
-                        </div>
-                    </md-card-content>
-
-                    <md-card-actions>
-                        <md-button v-if="event.status==='busy' || event.status==='new'" class="full-width md-primary" @click="goLive(event)" :md-ripple="false">Live</md-button>
-                        <md-button class="full-width">View Details</md-button>
-                    </md-card-actions>
-                </md-card>
-            </div>
-        </div>
-        <div style="height:100%;" v-if="eventLive">
-            <userlivemap :event="selectedEvent"></userlivemap>
-        </div>
+  <div>
+    <div v-if="myEvents.length == 0 && !eventLive" style="margin-top:20vh;">
+      <md-empty-state class="md-primary" md-icon="error" md-label="No events added yet" >
+        <md-button to="/report" class="stepper-next">Report now</md-button>
+      </md-empty-state>
     </div>
+    <div>
+      <div v-if="!eventLive" v-for="event in myEvents" :key="event.id" class="cards-layout md-layout-item md-size-25 md-medium-size-50 md-xsmall-size-100">
+        <md-card class="md-card-example">
+          <md-card-area md-inset>
+            <md-card-header>
+              <h2 class="md-title">{{event.requester.display}}</h2>
+              <div class="md-subhead">
+                <md-icon>location_on</md-icon>
+                <span>{{event.location}}</span>
+              </div>
+            </md-card-header>
+          </md-card-area>
+
+          <md-card-content>
+            <h3 class="md-subheading">Details</h3>
+            <div class="card-details">
+              <md-icon>access_time</md-icon>
+              <div class="md-button-group">
+                <span class="description-item">{{formatDate(event.createdAt)}}</span>
+              </div>
+            </div>
+            <div class="card-details">
+              <md-icon>local_offer</md-icon>
+              <div class="md-button-group">
+                <md-chip v-for="tag in event.tags" :key="tag.name" :class="getGravityClass(tag.gravity)">
+                  {{tag.name}}
+                </md-chip>
+              </div>
+            </div>
+          </md-card-content>
+
+          <md-card-actions>
+            <md-button v-if="event.status==='busy' || event.status==='requested'" class="full-width md-primary" @click="goLive(event)" :md-ripple="false">Live</md-button>
+            <md-button class="full-width">View Details</md-button>
+          </md-card-actions>
+        </md-card>
+      </div>
+    </div>
+    <div style="height:100%;" v-if="eventLive">
+      <userlivemap :event="selectedEvent"></userlivemap>
+    </div>
+  </div>
 </template>
 
 <style lang="sass" scoped>
